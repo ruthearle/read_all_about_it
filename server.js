abfadba73e77800fddeca8f67aacb23fdea24d65
Ruth Earle <rrearle@gmail.com>
2014-12-24 14:33:26 +0000

    Add a single endpoint that will display news feeds from db

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ var request = require('request');
 var parser = require('rssparser');
 var options = {};
 
+var mongoose= require('mongoose');
+mongoose.connect('mongodb://localhost/readAllAboutIt');
 
-var server = restify.createServer({
-  name: 'ReadAllAboutIt',
+/*var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'conection error:'));
+db.once('open', function (callback) {
+
+  var newsSchema = mongoose.Schema({
+    title: String,
+    summary: String,
+    url: String,
+    timeAgo: String
+  })
+
 });
+*/
 
-//set up default headers (fullResponse) and to reuse code (bodyParser)
-server.use(restify.fullResponse());
-server.use(restify.bodyParser());
+
 
 parser.parseURL('http://news.sky.com/feeds/rss/home.xml', options, function(err, sky){
   console.log(sky);
@@ -25,12 +35,18 @@ parser.parseURL('http://feeds.feedburner.com/TheHackersNews', options, function(
   console.log(hacker);
 });
 
-/*request('http://api.ihackernews.com/page', function(err, response, hacker) {
-  if (!err && response.statusCode == 200) {
-    console.log(hacker);
-  }
+var server = restify.createServer({
+  name: 'ReadAllAboutIt',
 });
-*/
+
+//set up default headers (fullResponse) and to reuse code (bodyParser)
+server.use(restify.fullResponse());
+server.use(restify.bodyParser());
+
+server.get("/", function(req, res, next) {
+  res.send('All news bulletins will be displayed with this endpoint');
+  return next();
+});
 
 server.listen(8080, function(){
   console.log('%s listening at %s', server.name, server.url);
